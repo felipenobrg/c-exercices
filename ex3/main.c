f1fce69b7f1fc67a15d208e13be74cfec2e07e18
Felipe Nóbrega <felipenobrega2012@gmail.com>
2023-09-12 14:35:52 -0300

    feat: calcuting the dividend, divisor, quocient

---
+++ ex3/main.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main() 
+{
+    int first_number, second_number, rest1, rest2;
+    float quotient1, quotient2;
+
+    printf("Escreva o primeiro numero: ");
+    scanf("%d", &first_number);
+
+    printf("Escreva o segundo numero: ");
+    scanf("%d", &second_number);
+
+    quotient1 = first_number / second_number;
+    quotient2 = second_number / first_number;
+
+    rest1 = first_number % second_number;
+    rest2 = second_number % first_number;
+
+    printf("Os numeros digitados sao: %d e %d\n", first_number, second_number);
+    printf("O primeiro pelo segundo: %.1f\n", quotient1);
+    printf("O segundo pelo primeiro: %.1f\n", quotient2);
+    printf("O resto do primeiro: %.1d\n", rest1);
+    printf("O resto do segundo: %.1d", rest2);
+    
+    return 0;
+}
