e642a32f5699208c0bc3c95a540a3708eb4bdd33
Felipe Nóbrega <felipenobrega2012@gmail.com>
2023-09-12 14:14:05 -0300

    feat: creating program to calculate average

---
+++ ex1/main.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main() 
+{
+    int grade1, grade2, grade3, grade4, grade5;
+    float average;
+    
+    printf("Digite o valor da primeira media: ");
+    scanf("%d", &grade1);
+    printf("Digite o valor da segunda media: ");
+    scanf("%d", &grade2);
+
+    printf("Digite o valor da terceira media: ");
+    scanf("%d", &grade3);
+
+    printf("Digite o valor da quarta media: ");
+    scanf("%d", &grade4);
+
+    printf("Digite o valor da quinta media: ");
+    scanf("%d", &grade5);
+
+    average = (grade1 + grade2 + grade3 + grade4 + grade5) / 5;
+    printf("Media: %.1f", average);
+
+    return 0;
+}
