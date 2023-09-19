cfa955278ebe0c0863184c55f26b2d5e410c147d
Felipe Nóbrega <felipenobrega2012@gmail.com>
2023-09-19 10:29:41 -0300

    feat: calculating the swuare and cube of a number

---
+++ ex7/ED-lista1-questao7.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main()
+/*
+** Função: Determinar resultado do quadrado e do cubo do número fornecido
+** Autor: Felipe Nóbrega de Almeida
+** Data: 19/09
+** Observações:
+*/
+{
+   double number, square, cube;
+
+   printf("Insira um numero real: ");
+   scanf("%lf", &number);
+
+   square = number * number;
+   cube = number * number * number;
+
+   printf("O quadrado de %.2lf é %.2lf\n", number, square);
+   printf("O cubo de %.2lf é %.2lf\n", number, cube);
+   return 0;
+}
