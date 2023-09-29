3eb39c2a0b93277118720939fc86547b2600d52a
Felipe Nóbrega <felipenobrega2012@gmail.com>
2023-09-29 17:25:37 -0300

    feat: checking if a year is bissexto or not

---
+++ work2/ex18/ED-lista2-questao18.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main()
+/*
+** Função: Verificando se um ano é bissexto ou não.
+** Autor: Felipe Nóbrega de Almeida
+** Data: 28/09
+** Observações:
+*/
+{
+    int user_year;
+
+    printf("Informe o ano: ");
+    scanf("%d", &user_year);
+  
+   if ((user_year % 400 == 0) || ((user_year % 4 == 0) && (user_year % 100 != 0))) {
+        printf("Eh um ano bissexto.");
+    } else {
+        printf("Nao e um ano bissexto.");
+    }
+
+    return 0;
+}
