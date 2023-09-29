5060fc7075f075c4d5b7bad0cf30f7e02075b62a
Felipe Nóbrega <felipenobrega2012@gmail.com>
2023-09-29 12:49:28 -0300

    fix: changing wrong number

--- work2/ex1/ED-lista2-questao1.c
+++ work2/ex1/ED-lista2-questao1.c
@@ -14,12 +14,12 @@ int main()
 
     factorial_5 = 5 * 4 * 3 * 2 * 1;
 
-    a = 2 + 3 - 5 * (8 / 4);
+    a = 2 + 3 - (5 * 8) / 4;
     b = (7 * 4) / 2 + 9 - 6;
     c = (4 / 2) / 0.5 * ((4 % 2)) * 0.5;
     d = 7 > 2 && 3 - 5 < 8;
     e = !(3 + factorial_5 == (5 / 2) - 1);
-    f = ((7 % 4)) - 8 + (4 / 2);
+    f = ((7 % 3)) - 8 + (4 / 2);
     g = (6 / 3) - 3 * ((4 % 2));
     h = 10 > 11 && 11 < 12;
     i = 9 / 3 + ((9 % 3)) + 9 * 3;
@@ -30,7 +30,7 @@ int main()
     printf("C: %.f, Resultado: 0 \n", c);
     printf("D: %.f, Resultado: 1\n", d);
     printf("E: %.f, Resultado: 1\n", e);
-    printf("F: %.f, Resultado: -3\n", f);
+    printf("F: %.f, Resultado: -5\n", f);
     printf("G: %.f, Resultado: 2\n", g);
     printf("H: %.f, Resultado: 0\n", h);
     printf("I: %.f, Resultado: 30\n", i);
