e47c2184d0c925e066f5a5ad08c9aad97ac2b409
Felipe Nóbrega <felipenobrega2012@gmail.com>
2023-11-11 20:00:27 -0300

    feat: formating code

--- work3/ex4/TDA-lista1N2-questao04.c
+++ work3/ex4/TDA-lista1N2-questao04.c
@@ -1,41 +1,51 @@
 #include <stdio.h>
 
 /*
-** Função: Programa informa se o número é ou não primo, 
-em seguida exibe todos os números primeiros entre 2 e o 
+** Função: Programa informa se o número é ou não primo,
+em seguida exibe todos os números primeiros entre 2 e o
 número inserido pelo usuário.
 ** Autor: Felipe Nóbrega de Almeida
 ** Data: 10/11
 ** Observações:
 */
 
-int ePrimo(int num) {
-  if (num <= 1) {
+int ePrimo(int num)
+{
+  if (num <= 1)
+  {
     return 0;
   }
-  for (int i = 2; i * i <= num; i++) {
-    if (num % i == 0) {
+  for (int i = 2; i * i <= num; i++)
+  {
+    if (num % i == 0)
+    {
       return 0;
-    } 
+    }
   }
   return 1;
 }
 
-int main() {
+int main()
+{
   int number;
   printf("Digite um numero inteiro positivo: ");
   scanf("%d", &number);
 
-  if (ePrimo(number)) {
+  if (ePrimo(number))
+  {
     printf("%d eh primo.\n", number);
-  } else {
+  }
+  else
+  {
     printf("%d nao eh primo.\n", number);
   }
 
-  for (int i = 2; i <= number; i++) {
-  if (ePrimo(i)) {
-    printf("%d ", i);
-   }
+  for (int i = 2; i <= number; i++)
+  {
+    if (ePrimo(i))
+    {
+      printf("%d ", i);
+    }
   }
   printf("\n");
 
