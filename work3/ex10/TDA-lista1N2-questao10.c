90b4a3ae18ff0b7a8e61c7eb75bbac0a2ead1638
Felipe Nóbrega <felipenobrega2012@gmail.com>
2023-11-14 19:52:34 -0300

    feat: finishing ex 10

---
+++ work3/ex10/TDA-lista1N2-questao10.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+/*
+** Função: Eleva número (base) a outro (expoente) usando
+função recursiva.
+** Autor: Felipe Nóbrega de Almeida
+** Data: 14/11
+** Observações:
+*/
+
+#include <stdio.h>
+
+float pot(float base, int expoente);
+
+int main() {
+    float base;
+    int expoente;
+
+    printf("Digite a base: ");
+    scanf("%f", &base);
+
+    printf("Digite o expoente: ");
+    scanf("%d", &expoente);
+
+    printf("%.2f elevado a %d é %.2f\n", base, expoente, pot(base, expoente));
+
+    return 0;
+}
+
+float pot(float base, int expoente) {
+    if (expoente == 0) {
+        return 1;
+    }
+    else {
+        return base * pot(base, expoente - 1);
+    }
+}
